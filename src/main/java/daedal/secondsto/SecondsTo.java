4d3a81449ae25a61174d673d77e5e52765a9ce21
Daedal <dev.icaruseffect@gmail.com>
2023-01-31 15:52:14 +0300

    Initial commit

---
+++ src/main/java/daedal/secondsto/SecondsTo.java
@@ -0,0 +1,55 @@
+package daedal.secondsto;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.List;
+
+public final class SecondsTo extends PlaceholderExpansion {
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return "secondsto";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return "Daedal";
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer p, @NotNull String params) {
+        if (params.length() != 5) {
+            return "ФОРМАТ ВРЕМЕНИ ДОЛЖЕН БЫТЬ XX:XX";
+        }
+        if (!params.contains(":")) {
+            return "ФОРМАТ ВРЕМЕНИ ДОЛЖЕН БЫТЬ XX:XX";
+        }
+        LocalDateTime currentTime = LocalDateTime.now();
+        int currentHour = currentTime.getHour();
+        int currentMinute = currentTime.getMinute();
+        List<String> toList = Arrays.asList(params.split(":"));
+        int targetHour = Integer.parseInt(toList.get(0));
+        int targetMinute = Integer.parseInt(toList.get(1));
+        LocalDateTime targetTime;
+        if (targetHour < currentHour && targetMinute < currentMinute) {
+            targetTime = currentTime.plusDays(1).withHour(targetHour).withMinute(targetMinute);
+        }
+        else {
+            targetTime = currentTime.withHour(targetHour).withMinute(targetMinute);
+        }
+        long unixCurrent = currentTime.toEpochSecond(ZoneOffset.UTC);
+        long unixTarget = targetTime.toEpochSecond(ZoneOffset.UTC);
+        long diff = unixTarget - unixCurrent;
+        return diff + "s";
+    }
+}
